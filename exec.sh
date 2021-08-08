d6cc89ecba042b6b8feefb901f6d7dd6fead74f0
Hiroyuki Miyazaki <heromiya@heromiya.net>
2021-08-08 21:46:23 +0700

    modified:   exec.sh
    	modified:   tuto.py

--- exec.sh
+++ exec.sh
@@ -4,6 +4,7 @@ BATCH_SIZE=$1
 DROPOUT=$2
 TARGET=$3
 OPTIMIZER=$4
+PRETRAINED=$5
 
 BACKBONE=resnext101
 EPOCHS=400
@@ -12,11 +13,12 @@ LR=0.001
 LOSS=jd
 ACTIVATION=hard_sigmoid
 
-BASENAME=$TARGET-$MODEL-$BACKBONE-$DROPOUT-$BATCH_SIZE-$OPTIMIZER-$LR-$LOSS-$ACTIVATION
+BASENAME=$(date +%F_%T)-$TARGET-$MODEL-$BACKBONE-$DROPOUT-$BATCH_SIZE-$OPTIMIZER-$LR-$LOSS-$ACTIVATION
 CHECKPOINT=checkpoints/$BASENAME.h5
-LOG=logs/$(date +%F_%T)-$BASENAME.log
+LOG=logs/$BASENAME.log
 
 mkdir -p logs checkpoints
+
 
 python tuto.py --batch_size $BATCH_SIZE \
        --dropout $DROPOUT \
@@ -28,4 +30,5 @@ python tuto.py --batch_size $BATCH_SIZE \
        --optimizer $OPTIMIZER \
        --lr $LR \
        --loss $LOSS \
-       --activation $ACTIVATION 2>&1 | tee $LOG
+       --activation $ACTIVATION \
+       --pretrained $PRETRAINED 2>&1 | tee $LOG
